06f5905aa636184e602ea4db3a38d195215f2453
Adam Bishop <adam.michael.bishop@gmail.com>
2023-01-31 13:49:56 -0600

    Finished problem 1

--- solutions.js
+++ solutions.js
@@ -7,3 +7,15 @@
  * immediately invoked function expression (iife), you should _not_ do that
  * here. If you do, the automated tests will not be able to see your functions.
  */
+
+// 1.
+const isBoolean = testCase => typeof testCase === 'boolean';
+
+console.log(isBoolean('blue'),
+isBoolean([9,0,'red']),
+isBoolean(true),
+isBoolean(false),
+isBoolean('true'),
+isBoolean(1));
+
+// 2.
